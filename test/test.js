c1de334405256da801b65da5f3b46b4415babb31
KeyangXiang <keyang.xiang@gmail.com>
2016-04-18 12:01:58 +0100

    add callbacks; updated tests

--- test/test.js
+++ test/test.js
@@ -15,39 +15,22 @@ function getFunc(dummy) {
 describe("Influxdb udp connector", function() {
   it("should send udp packet following line protocol", function() {
     var mock = {
-      send: sinon.spy(),
+      send: sinon.stub(),
       close: sinon.spy()
     }
     var func = getFunc(mock);
+    var cb=sinon.spy()
     func({
       host:"127.0.50.11",
       port:3556,
       tags:"cpu",
       values:"value=50"
-    });
+    },cb);
+    mock.send.yield([]);
     sinon.assert.calledOnce(mock.send);
+    sinon.assert.calledOnce(cb);
     var buf=new Buffer("cpu value=50\n");
     sinon.assert.calledWith(mock.send,buf,0,buf.length,3556,"127.0.50.11");
     sinon.assert.calledOnce(mock.close);
-  });
-
-  it ("should specifically say not closing it",function(){
-    var mock = {
-      send: sinon.spy(),
-      close: sinon.spy()
-    }
-    var func = getFunc(mock);
-    func({
-      host:"127.0.50.11",
-      port:3556,
-      tags:"cpu,project=pro1",
-      values:"value=50",
-      timestamp:"123456789s",
-      close:false
-    });
-    sinon.assert.calledOnce(mock.send);
-    var buf=new Buffer("cpu,project=pro1 value=50 123456789s\n");
-    sinon.assert.calledWith(mock.send,buf,0,buf.length,3556,"127.0.50.11");
-    sinon.assert.notCalled(mock.close);
   });
 });
